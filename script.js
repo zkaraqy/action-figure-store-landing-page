e9f052b1bbadb167094fb981caf729a3bd8de93c
Azka Raki <azkaraqy552@gmail.com>
2024-02-05 13:13:43 +0800

    refactoring code

--- script.js
+++ script.js
@@ -1,53 +1,44 @@
 const menuToggle = document.querySelector(".menu input");
 const nav = document.querySelector(".navigation .links");
-const hero = document.querySelector(".preview.Albedo");
-const next = document.querySelector(
-  ".Albedo .next",
-  ".Asuna .next",
-  ".Megumi .next"
-);
-const previous = document.querySelector(
-  ".Albedo .previous",
-  ".Asuna .previous",
-  ".Megumi .previous"
-);
-const asunaNext = document.querySelector(
-  ".preview_home .actionFigure .preview.Asuna .next"
-);
-const asunaPrevious = document.querySelector(
-  ".preview_home .actionFigure .preview.Asuna .previous"
-);
-const megumiNext = document.querySelector(
-  ".preview_home .actionFigure .preview.Megumi .next"
-);
-const megumiPrevious = document.querySelector(
-  ".preview_home .actionFigure .preview.Megumi .previous"
-);
-const home = document.querySelector(".links a");
-
 
 menuToggle.addEventListener("click", function () {
   nav.classList.toggle("slide");
 });
-next.addEventListener("click", function () {
-  hero.classList.add("remove");
-});
-previous.addEventListener("click", function () {
-  hero.classList.add("remove");
-});
-asunaPrevious.addEventListener("click", function () {
-  hero.classList.remove("remove");
-});
-asunaNext.addEventListener("click", function () {
-  hero.classList.add("remove");
-});
-megumiNext.addEventListener("click", function () {
-  hero.classList.remove("remove");
-});
-megumiPrevious.addEventListener("click", function () {
-  hero.classList.add("remove");
+
+const imgPreview = document.querySelector(".preview");
+const nextButton = document.querySelector(".next");
+const previousButton = document.querySelector(".previous");
+
+nextButton.addEventListener("click", function () {
+  if (imgPreview.getAttribute("src") === "image action figure/albedo.png") {
+    imgPreview.setAttribute("src", "image action figure/asuna.png");
+    imgPreview.setAttribute("title", "asuna");
+  } else if (
+    imgPreview.getAttribute("src") === "image action figure/asuna.png"
+  ) {
+    imgPreview.setAttribute("src", "image action figure/yukino.png");
+    imgPreview.setAttribute("title", "yukino");
+  } else if (
+    imgPreview.getAttribute("src") === "image action figure/yukino.png"
+  ) {
+    imgPreview.setAttribute("src", "image action figure/albedo.png");
+    imgPreview.setAttribute("title", "albedo");
+  }
 });
 
-home.addEventListener("click", function() {
-  hero.classList.remove("remove");
-})
+previousButton.addEventListener("click", function () {
+  if (imgPreview.getAttribute("src") === "image action figure/albedo.png") {
+    imgPreview.setAttribute("src", "image action figure/yukino.png");
+    imgPreview.setAttribute("title", "yukino");
+  } else if (
+    imgPreview.getAttribute("src") === "image action figure/asuna.png"
+  ) {
+    imgPreview.setAttribute("src", "image action figure/albedo.png");
+    imgPreview.setAttribute("title", "albedo");
+  } else if (
+    imgPreview.getAttribute("src") === "image action figure/yukino.png"
+  ) {
+    imgPreview.setAttribute("src", "image action figure/asuna.png");
+    imgPreview.setAttribute("title", "asuna");
+  }
+});
